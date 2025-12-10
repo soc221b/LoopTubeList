a43b7cf861fa394bcfbff515533b81274578c895
Ernest <soc221b.e@gmail.com>
2025-12-10 14:03:44 +0800

    test(a11y): install jest-axe and stub canvas getContext for jsdom

--- jest.setup.ts
+++ jest.setup.ts
@@ -2,4 +2,19 @@ import '@testing-library/jest-dom';
 
 // Additional global test setup can go here.
 
+// Some axe-core checks call canvas APIs not implemented by jsdom. Provide
+// a minimal stub so accessibility checks run in the test environment.
+if (typeof HTMLCanvasElement !== 'undefined' && !HTMLCanvasElement.prototype.getContext) {
+	// @ts-ignore
+	HTMLCanvasElement.prototype.getContext = function () {
+		return {
+			getImageData: () => ({ data: [] }),
+			putImageData: () => {},
+			createImageData: () => [],
+			getContextAttributes: () => ({}),
+			measureText: () => ({ width: 0 }),
+		} as any;
+	};
+}
+
 export {};
